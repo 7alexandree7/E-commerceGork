bb79ee41d6c39a9ad1bebecbd49fa75a289054ce
alexandre <alexandre020602@gmail.com>
2025-07-24 20:06:41 -0300

    Finalizando os estilos por hora do Head

--- src/components/layout/Head/Head.jsx
+++ src/components/layout/Head/Head.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import './Head.css'
 
 const Head = () => {
-  return (
-    <div>
-      Head
-    </div>
-  )
+    return (
+        <>
+            <header className='head'>
+                <div className="container d_flex">
+
+                    <div className="left row">
+                        <i className='fa fa-phone'></i>
+                        <label>+88012 3457 7894</label>
+                        <i className='fa fa-envelope'></i>
+                        <label>example@gmail.com</label>
+                    </div>
+
+                    <div className="right row RText">
+                        <label>Theme FAQ ' s</label>
+                        <label>Nedd Helps</label>
+                        <span>🏳️‍⚧️</span>
+                        <label>EN</label>
+                        <span>🏳️‍⚧️</span>
+                        <label>USD</label>
+                    </div>
+
+                </div>
+            </header>
+        </>
+    )
 }
 
 export default Head
